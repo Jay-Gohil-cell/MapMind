fa68345affed107769cf2c41eaa05be71137f7e6
Jay Gohil <gohiljay408@gmail.com>
2026-06-04 00:16:57 +0530

    make footer responsive

--- src/assets/Components/Footer.jsx
+++ src/assets/Components/Footer.jsx
@@ -3,20 +3,20 @@ import backBtn from '../images/bold-arrow-top-icon.svg'
 
 const Footer = () => {
     return (
-        <section id="Footer" className="h-auto w-full bg-black py-10 px-20 text-white py-4 border-t">
-            <div className='flex justify-between sm:flex-col lg:flex-row items-center mb-5'>
+        <section id="Footer" className="h-auto w-full bg-black px-8 md:px-20 text-white py-10 border-t">
+            <div className='flex justify-between flex-row items-center mb-5'>
                 <ul className='flex gap-1 items-center'>
-                    <img src={logoImg} alt="Logo" className='w-10 h-10 grayscale-50' />
-                    <div className="font-outfit text-4xl font-semibold pt-1 text-white tracking-wide z-50">
+                    <img src={logoImg} alt="Logo" className='w-8 h-8 md:w-10 md:h-10 grayscale-50' />
+                    <div className="font-outfit text-3xl md:text-4xl font-semibold md:pt-1 text-white tracking-wide z-50">
                         Map<span className="text-slate-400">Mind</span>
                     </div>
                 </ul>
 
                 <a
                     href="#Hero"
-                    className="flex items-center gap-3 group"
+                    className="flex items-center gap-2 md:gap-3 group"
                 >
-                    <span className='text-2xl font-semibold uppercase font-outfit'>Back to top</span>
+                    <span className='text-xl font-semibold uppercase font-outfit'>Back to top</span>
 
                     <img
                         src={backBtn}
@@ -26,8 +26,8 @@ const Footer = () => {
                 </a>
             </div>
 
-            <div className='flex justify-between sm:flex-col lg:flex-row items-start pb-8'>
-                <p className='text-gray-400 w-100 text-justify'>An interactive geographic dashboard designed to visualize global data, discover unique insights, and explore world facts seamlessly.</p>
+            <div className='flex justify-between flex-col items-center lg:flex-row gap-8 lg:gap-0 lg:items-start pt-8 lg:pt-0 pb-8'>
+                <p className='text-gray-400 md:max-w-200 lg:max-w-100 tracking-wider text-justify'>An interactive geographic dashboard designed to visualize global data, discover unique insights, and explore world facts seamlessly.</p>
 
                 <ul className='font-light uppercase flex gap-5'>
                     <li className='text-gray-400 hover:text-white'>Home</li>
